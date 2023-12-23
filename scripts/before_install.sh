68874a4e9409a13f159eb5a892c371caeaba4927
Anas Siddiqui <connect.anas.dev@gmail.com>
2023-12-23 18:13:44 +0530

    edited before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# #download node and npm
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-# . ~/.nvm/nvm.sh
-# nvm install 16.16.0
-
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/express-app"
 if [ -d "$DIR" ]; then
@@ -13,6 +8,13 @@ else
   echo "Creating ${DIR} directory"
   mkdir ${DIR}
 fi
+
+#download node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16.16.0
+
+
 
 # #download node and npm
 # sudo yum install nodejs npm --enablerepo=epel
